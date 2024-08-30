67647ceee31befc6d379a5369a61e4e68d129603
luanabrizola <130062674+luanabrizola@users.noreply.github.com>
2024-08-30 09:25:31 -0300

    TaskList e itens

--- src/componentes/Main/Main.jsx
+++ src/componentes/Main/Main.jsx
@@ -1,11 +1,33 @@
 import styles from "./Main.module.css"
 
 
-function Main(props){
-    let {children} = props
-    return(
+
+
+function Main(props) {
+    // let {children} = props
+    return (
         <div className={styles.Main}>
-            {children}
+            {/* {children} */}
+            <div className={styles.input}>
+                <input type="text" />
+                <button className={styles.botaoAdd}>+</button>
+            </div>
+            <div className={styles.itens}>
+                <div className={styles.task}>
+                    <p>Item 1</p>
+                    <button className={styles.buttonX}>X</button>
+                </div>
+                <div className={styles.task}>
+                    <p>Item 2</p>
+                    <button className={styles.buttonX}>X</button>
+                </div>
+                <div className={styles.task}>
+                    <p>Item 3</p>
+                    <button className={styles.buttonX}>X</button>
+                </div>
+
+            </div>
+            
         </div>
 
     )
